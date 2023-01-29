b58fd190a8e91b31fe9f527d932db33b626c487b
AkeenL1 <akeenlewis1@gmail.com>
2023-01-28 21:20:27 -0500

    fixing font

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,19 +7,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: `gatsby-omni-font-loader`,
-      options: {
-        enableListener: true,
-        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
-        web: [
-          {
-            name: `Roboto`,
-            file: `https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap`,
-          },
-        ],
-      },
-    },
     'gatsby-plugin-postcss',
   ],
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import "../styles/global.css"
 
 const NavBar = ({link1,link2,link3,link4}) => {
     return (
-        <nav className="h-fit border-b-4 text-xl" >
+        <nav className="font-serif h-fit border-b-4 text-xl" >
             <ul className="pl-20 pb-4 flex flex-row space-x-8">
                 <li>{link1}</li>
                 <li>{link2}</li>
